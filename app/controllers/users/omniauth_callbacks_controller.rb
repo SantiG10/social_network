4db9a9903061408e2677954eba1f4258f35136aa
Santiago Giraldo M <santi_rm8@hotmail.com>
2016-10-26 14:26:42 -0500

    add edit user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,23 @@ class Users::OmniauthCallbacksController < ApplicationController
     if @user.persisted?
       @user.remember_me = true
       sign_in_and_redirect @user, event: :authentication
+      return
+    end
+    session["devise.auth"] = request.env["omniauth.auth"]
+    render :edit
+  end
+
+  def custom_sign_up
+    @user = User.from_omniauth(session["devise.auth"])
+    if @user.update(user_params)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      render :edit
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username,:name,:email)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
+    where(provider:auth[:provider], uid:auth[:uid]).first_or_create do |user|
       if auth[:info]
         user.email = auth[:info][:email]
         user.name = auth[:info][:name]
